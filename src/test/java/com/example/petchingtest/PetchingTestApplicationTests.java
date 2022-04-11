bcadf3ff8cae57c3a937017260dcfb60127f4c32
seungyeonpark <siaayeon@gmail.com>
2022-04-11 14:08:07 +0900

    settings

---
+++ src/test/java/com/example/petchingtest/PetchingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petchingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetchingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
